18a301c2ab95cdc939684615ff3ad18a9ca983dd
Dustin Brett <dustin@spintechit.ca>
2018-11-16 11:22:48 -0800

    1 less sapply

--- R/model-setup.R
+++ R/model-setup.R
@@ -41,15 +41,13 @@ model_structure <- function(txseqs,
     stop("If the model starts at second line, then n_states must be 'three'.",
          call. = FALSE)
   }
-  cl <- parallel::makeCluster(parallel::detectCores())
-  if (n_states == "four" & "osimertinib" %in% parallel::parSapply(cl = cl, txseqs, function (x) x$first)){
+  if (n_states == "four" & "osimertinib" %in% sapply(txseqs, function (x) x$first)){
       msg <- paste0("There is no evidence to parameterize a model with four health states ", 
                     "when osimertinib is used as a first line treatment. ",
                     "Use a model with three health states for sequences beginning ",
                     "with osimertinib instead.")
       stop(msg, call. = FALSE) 
   }
-  parallel::stopCluster(cl)
   l <- list(txseqs = txseqs,
             n_states = n_states,
             dist = dist)
