3b08327b805daf7c737d8ffc4017be47e3f49d82
Dustin Brett <dustin@spintechit.ca>
2018-11-16 14:08:23 -0800

    fsdfsd

--- R/mcda.R
+++ R/mcda.R
@@ -112,6 +112,7 @@ mcda <- function(x, sample, strategy, criteria,
   
   if (is.null(criteria_min)){
     criteria_min <- rep(NA, length(criteria))
+    library(foreach)
     criteria_min <- foreach (i=1:length(criteria)) %do% ifelse(optimal[i] == "low", max(x[[criteria[i]]]), min(x[[criteria[i]]]))
   }
   if (is.null(criteria_max)){
